b6ad4562e82d5a346a87e74ee8f774fa331b30fc
nickd-bhsc <duannd@myumanitoba.ca>
2022-04-15 17:21:21 -0500

    added boulder check

--- main.py
+++ main.py
@@ -19,24 +19,24 @@ def bellman(state, grid, x, y):
         for a in state.actions:
             value = 0
             if a == 'S': # probability, utility, 1-probability, utility
-                if y-1 >= 0:
+                if y-1 >= 0 and grid.grid[x][y-1].symbol != 'B':
                     value += correctDirProb * (state.cost + discount * grid.grid[x][y-1].utility )
                 else:
                     value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if x-1 >= 0:
+                if x-1 >= 0 and grid.grid[x-1][y].symbol != 'B':
                     value += slipProb * (state.cost + discount * grid.grid[x-1][y].utility )
                 else:
-                    value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if x+1 < grid.width:
+                    value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
+                if x+1 < grid.width and grid.grid[x+1][y].symbol != 'B':
                     value += slipProb * (state.cost + discount * grid.grid[x+1][y].utility ) # + DISCOUNTED UTILITY OF S'v
                 else:
                     value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
             if a == 'N':
-                if y+1 < grid.height:
+                if y+1 < grid.height and grid.grid[x][y+1].symbol != 'B':
                     value += correctDirProb* (state.cost + discount * grid.grid[x][y+1].utility )
                 else:
                     value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if x-1 >= 0:
+                if x-1 >= 0 and grid.grid[x-1][y].symbol != 'B':
                     value += slipProb * (state.cost + discount * grid.grid[x-1][y].utility )
                 else:
                     value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
@@ -45,28 +45,28 @@ def bellman(state, grid, x, y):
                 else:
                     value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
             if a == 'E':
-                if x+1 < grid.width:
+                if x+1 < grid.width and grid.grid[x+1][y].symbol != 'B':
                     value += correctDirProb * (state.cost + discount * grid.grid[x+1][y].utility )
                 else:
                     value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if y+1 < grid.height:
+                if y+1 < grid.height and grid.grid[x][y+1].symbol != 'B':
                     value += slipProb * (state.cost + discount * grid.grid[x][y+1].utility )
                 else:
-                    value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if y-1 >= 0:
+                    value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
+                if y-1 >= 0 and grid.grid[x][y-1].symbol != 'B':
                     value += slipProb * (state.cost + discount *  grid.grid[x][y-1].utility )# + DISCOUNTED UTILITY OF S'v
                 else:
                     value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
             if a == 'W':
-                if x-1 >= 0:
+                if x-1 >= 0 and grid.grid[x-1][y].symbol != 'B':
                     value += correctDirProb * (state.cost + discount * grid.grid[x-1][y].utility) 
                 else:
                     value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if y+1 < grid.height:
+                if y+1 < grid.height and grid.grid[x][y+1].symbol != 'B':
                     value += slipProb * (state.cost + discount *  grid.grid[x][y+1].utility )
                 else:
-                    value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
-                if y-1 >=0:
+                    value += correctDirProb * (state.cost + discount * grid.grid[x][y].utility) 
+                if y-1 >=0 and grid.grid[x][y-1].symbol != 'B':
                     value += slipProb * (state.cost + discount * grid.grid[x][y-1].utility )# + DISCOUNTED UTILITY OF S'v
                 else:
                     value += slipProb * (state.cost + discount * grid.grid[x][y].utility) 
